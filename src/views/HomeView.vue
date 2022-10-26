9e273cdd3afb1accf714a9673d9ac56ff945dfa3
GarlicBuns19 <planterx26@gmail.com>
2022-10-26 09:00:19 +0200

    RPS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home page for rps</h1>
+
+    <div class="hand">
+      <div>
+        <button>Rock</button>
+        <button>Paper</button>
+        <button>Scissors</button>
+      </div>
+
+      <div>
+        P1
+      </div>
+
+      <div>
+        P2
+      </div>
+
+      <div>
+        <button>Rock</button>
+        <button>Paper</button>
+        <button>Scissors</button>
+      </div>
+    </div>
+    
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.home{
+  max-width: 700px;
+  margin: 0 auto;
 }
+.hand{
+  display: flex;
+  /* flex-flow: column; */
+  justify-content: space-around;
+  align-items: center;
+}
+.hand button {
+  margin: 20px auto;
+  padding: 20px;
+  display: block;
+  width: 100px;
+  /* height: 100px; */
+  box-sizing: border-box;
+  /* align-self: center; */
+}
+</style>
+
+<script>
+export default {};
 </script>
