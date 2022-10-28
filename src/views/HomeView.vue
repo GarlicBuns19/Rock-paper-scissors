b98de47f0bf5cbd6aee3e76f0321ee712fa7f302
GarlicBuns19 <planterx26@gmail.com>
2022-10-28 09:33:04 +0200

    It works

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,17 +6,17 @@
       <div>
         <button @click="inhand('rock1')" id="rock1">Rock</button>
         <button @click="inhand('paper1')" id="paper1">Paper</button>
-        <button @click="inhand('sciccors1')" id="sciccors">Scissors</button>
+        <button @click="inhand('scissors1')" id="scissors">Scissors</button>
       </div>
 
-      <div id="player1">P1 = {{ output1 }}</div>
+      <div id="player1">P1 = <span>{{ output1 }}</span></div>
 
       <div id="player2">{{ output2 }} = P2</div>
 
       <div>
-        <button @click="inhand('rock2') " id="rock2">Rock</button>
+        <button @click="inhand('rock2')" id="rock2">Rock</button>
         <button @click="inhand('paper2')" id="paper2">Paper</button>
-        <button @click="inhand('scissors3')" id="scissors3">Scissors</button>
+        <button @click="inhand('scissors2')" id="scissors3">Scissors</button>
       </div>
     </div>
   </div>
@@ -51,21 +51,59 @@ export default {
   setup() {
     const output1 = ref("");
     const output2 = ref("");
-    const play = ref(false)
+    const play = ref(false);
     const p1 = document.getElementById("player1");
     const p2 = document.getElementById("player2");
-    
-    const inhand = (rps) => {
-      console.log(rps);
-      if (rps.includes('1')) {
-        output1.value = rps;
-      }
-      if (rps.includes('2')) {
-        output2.value = rps;
-      }
+
+    const whoWins = () => {
+      setTimeout(() => {
+        output1.value = "";
+        output2.value = "";
+      }, 1000);
     };
 
-    return { inhand, output1 , output2 , play };
+    const inhand = (rps) => {
+      console.log(rps);
+      if (rps.includes("1")) {
+        output1.value = rps.replace("1", "");
+      }
+      if (rps.includes("2")) {
+        output2.value = rps.replace("2", "");
+      }
+      (function checking() {
+        if (output1.value == output2.value) {
+          alert("It is Draw");
+          whoWins();
+        } else if (output1.value == "rock" && output2.value == "paper") {
+          alert("Player 2 WIns");
+          whoWins();
+        } else if (output1.value == "rock" && output2.value == "scissors") {
+          alert("Player 1 WIns");
+          whoWins();
+        } else if (output1.value == "paper" && output2.value == "scissors") {
+          alert("Player 2 WIns");
+          whoWins();
+        } else if (output1.value == "paper" && output2.value == "rock") {
+          alert("Player 1 WIns");
+          whoWins();
+        } else if (output1.value == "scissors" && output2.value == "paper") {
+          alert("Player 1 WIns");
+          whoWins();
+        } else if (output1.value == "scissors" && output2.value == "rock") {
+          alert("Player 2 WIns");
+          whoWins();
+        } 
+      })();
+    };
+
+    return { inhand, output1, output2, play };
+    // else if (output1.value == "paper" && output2.value == "scissors") {
+    //       alert("Player 2 WIns");
+    //       whoWins();
+    //     } else if (output1.value == "scissors" && output2.value == "rock") {
+    //       alert("Player 2 WIns");
+    //       whoWins();
+    //     }
   },
 };
 </script>
